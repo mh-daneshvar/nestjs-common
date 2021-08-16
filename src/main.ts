61a994ca081cf9e5e144961306ded45a5ad70283
mh-daneshvar <daneshvar.email@gmail.com>
2021-08-16 22:26:15 +0300

    fix(git): #WIP checking running husky before any new commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const jafar = '1234';
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
