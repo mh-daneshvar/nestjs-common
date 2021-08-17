711c2e3b0a965dec5c94fdd77e987b4135dce67b
mh-daneshvar <daneshvar.email@gmail.com>
2021-08-18 01:20:08 +0300

    feat(response-decorator): create an interceptor for decorating the response

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseDecorator } from './common/response-decorator/responseDecorator.interceptor';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalInterceptors(new ResponseDecorator());
+
+  // Load ConfigService
+  const servicePort = app.get(ConfigService).get<string>('SERVICE_PORT');
+
+  await app.listen(servicePort);
 }
 
 bootstrap();
