6aa5a812fbaf8dd755e63e2b5452d43b468920bb
mh-daneshvar <daneshvar.email@gmail.com>
2021-08-18 23:15:22 +0300

    feat(exceptions): create a global exception handler

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseDecorator } from './common/response-decorator/responseDecorator.interceptor';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Response Decorator
   app.useGlobalInterceptors(new ResponseDecorator());
+
+  // Exception Handler
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   // Load ConfigService
   const servicePort = app.get(ConfigService).get<string>('SERVICE_PORT');
