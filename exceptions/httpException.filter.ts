9359f8936a0775190af3cccde8a8da95819e2a53
mh-daneshvar <daneshvar.email@gmail.com>
2021-09-16 16:33:28 +0300

    doc(exceptions): add a simple function doc to "catch" method

--- exceptions/httpException.filter.ts
+++ exceptions/httpException.filter.ts
@@ -9,6 +9,11 @@ import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  /**
+   *
+   * @param exception
+   * @param host
+   */
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
